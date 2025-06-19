9b67aef86c96dfe085065eede1ec154c78dd49c8
ankur-b <ankurbarve29@gmail.com>
2025-06-19 19:59:29 +0530

    Rendering Questions and Registering Answers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Quiz from './components/Quiz'
 function App() {
   return (
-    <div className="App">
+    <>
       <Header/>
-    </div>
+      <main>
+        <Quiz/>
+      </main>
+    </>
   );
 }
 
--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,11 +1,32 @@
-import React,{ useState } from "react"
+import React, { useState } from "react";
+import Questions from "../questions";
 
-const Quiz = ()=>{
-    const [activeQuestionIndex,setActiveQuestionIndex] = useState(0)
-    const [userAnswers,setUserAnswers] = useState([])
-    return(
-        <p>
-            Current Active Question
-        </p>
-    )
-}
+const Quiz = () => {
+  const [userAnswers, setUserAnswers] = useState<string[]>([]);
+  const activeQuestionIndex = userAnswers.length;
+  const handleSelectAnser = (selectedAnswer: string) => {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  };
+  return (
+    <div id="quiz">
+      <div className="question">
+        <h2>{Questions[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {Questions[activeQuestionIndex].answers.map((answer) => {
+            return (
+              <li key={answer} className="answer">
+                <button onClick={() => handleSelectAnser(answer)}>
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;
